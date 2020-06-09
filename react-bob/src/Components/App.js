3725bf5f3ac1ae4ea9d8c8d1dab1f989216eadc4
“Milo <milogirardet@gmail.com>
2020-06-09 15:39:17 +0100

    initial commit

--- react-bob/src/Components/App.js
+++ react-bob/src/Components/App.js
@@ -1,11 +1,55 @@
 import React from 'react';
 import './App.scss';
 
+class Biscuit extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            allBiscuits:[],
+            top3Biscuits:[],
+            fourToTenBiscuits:[]
+        }
 
-// function App() {
-//   return (
+        //populates biscuits with data from api to create an array of objects
+        this.allBiscuits()
+        this.top3Biscuit()
+        this.fourToTenBiscuits()
+    }
 
-//   );
-// }
+    allBiscuits = () => {
+        fetch('http://localhost:3007/biscuits/')
+        .then(data => data.json())
+            .then((data) => {
+                this.setState({
+                    allBiscuits: data,
+                })
+            })
+    }
 
-// export default App;
+    top3Biscuits = () => {
+        fetch('http://localhost:3007/biscuits/top3')
+        .then(data => data.json())
+            .then((data) => {
+                this.setState({
+                    top3Biscuits: data,
+                })
+            })
+    }
+
+    fourToTenBiscuits = () => {
+        fetch('http://localhost:3007/biscuits/4-10')
+        .then(data => data.json())
+            .then((data) => {
+                this.setState({
+                    fourToTenBiscuits: data,
+                })
+            })
+    }
+
+    render() {
+        return (
+        
+            <Biscuit ={this.state.missions}/>
+        )
+        }
+    }
