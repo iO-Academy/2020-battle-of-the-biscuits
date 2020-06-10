9451061f8bb2ee20f2221c4f12e9b61969df2f89
Hannah Golightly <hannah.golightly@hotmail.co.uk>
2020-06-10 10:01:26 +0100

    header now displays, includes media queries

--- react-bob/src/Components/Header/Header.js
+++ react-bob/src/Components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './Header.scss';
 // import Logo from "./oreo-biscuit-recipe-cookie.jpg"
 
 class Header extends React.Component {
     render () {
         return (
-            <div className="header">
-                <img src={process.env.PUBLIC_URL + '/Images/Logo_biscuit.svg'} />
+            <div className="banner_container" >
+                <div className="banner">
+                <img className="logo" src={process.env.PUBLIC_URL + './Images/Logo_biscuit.png'} alt ="logo"/>
+                </div>
             </div>
         )
     }
