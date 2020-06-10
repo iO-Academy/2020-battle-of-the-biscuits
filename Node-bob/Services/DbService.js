7eec3a427869e6d6781648491cec4398c4c87952
Isabella Aston <isabella.aston8@gmail.com>
2020-06-10 16:03:00 +0100

    code review changes

--- Node-bob/Services/DbService.js
+++ Node-bob/Services/DbService.js
@@ -1,15 +1,15 @@
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient
 
-const url = 'mongodb://localhost:27017';
-const dbname = 'battle-of-the-biscuits';
+const url = 'mongodb://localhost:27017'
+const dbname = 'battle-of-the-biscuits'
 
-const Client = new MongoClient(url, {userNewUrlParser: true, useUnifiedTopology: true});
+const Client = new MongoClient(url, {userNewUrlParser: true, useUnifiedTopology: true})
 
 const connectToDb = (callback) => {
     Client.connect(() => {
-        let db = Client.db(dbname);
-        callback(db);
+        let db = Client.db(dbname)
+        callback(db)
     })
 }
 
-module.exports = connectToDb;
+module.exports = connectToDb
--- Node-bob/start.js
+++ Node-bob/start.js
@@ -1,9 +1,9 @@
 // This is the file you actually need to start the app
 // Run nodemon on this file
 
-const app = require('./app');
+const app = require('./app')
 
-const port = 9000;
+const port = 9000
 app.listen(port, () => {
-    console.log('listening');
-});
+    console.log('listening')
+})
