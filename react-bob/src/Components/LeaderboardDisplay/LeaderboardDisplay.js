1a254d050c8d0e3c181cbf5b7834262d1004e6aa
Hannah Golightly <hannah.golightly@hotmail.co.uk>
2020-06-11 12:15:01 +0100

    amends to routing of react file- adding in leaderboard.js

--- react-bob/src/Components/LeaderboardDisplay/LeaderboardDisplay.js
+++ react-bob/src/Components/LeaderboardDisplay/LeaderboardDisplay.js
@@ -1,32 +1,50 @@
 import React from 'react'
-import Header from '../Header/Header'
 
 class LeaderboardDisplay extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-        topThree: [],
-        fourToTen: []
-    }
-}
+    constructor(props) {
+      super(props)
+      this.state = {
+          topThree: [],
+          fourToTen: []
+      }
+  }
 
-componentDidUpdate(prevProps) {
-    if (this.props.allBiscuits !== prevProps.allBiscuits) {
+  componentDidUpdate(prevProps) {
+    if ((this.props.topThreeBiscuits !== prevProps.topThreeBiscuits) && (this.props.fourToTenBiscuits !== prevProps.fourToTenBiscuits)) {
         this.setState({
-            biscuitsToDisplay: this.props.allBiscuits
+            topThree: this.props.topThreeBiscuits,
+            fourToTen: this.props.fourToTenBiscuits
         })
     }
-}
+  }
+
   
   render () {
         return (
-          <div>          
-            <Header />
-            <h3>top10</h3>
-          </div>
+          <div>       
+            <h3>Top Ten Leaderboard</h3>
+            <div className= "leaderboard">
+              <div className="top3">
+                {this.state.topThree[0]}
+              </div>
+              <div className="top3">
+                {this.state.topThree[1]}
 
+              </div>
+              <div className="top3">
+                {this.state.topThree[2]}
+              </div>
+              {
+                this.state.fourToTen.map((biscuitFourToTen) => 
+                    <div class="biscuitFourToTenCard">
+                        <h2 class="biscuitFourToTenHeading" >{biscuitFourToTen.biscuitFourToTen_name}</h2>
+                        <p class="biscuitFourToTenText">{biscuitFourToTen.winratio}%</p>
+                    </div>   
+                )}
+            </div>
+          </div>
         )
-    }
+  }
 }
 
 export default LeaderboardDisplay
--- react-bob/src/index.js
+++ react-bob/src/index.js
@@ -4,7 +4,7 @@ import {Route, BrowserRouter as Router,Switch} from 'react-router-dom'
 import './index.scss'
 import NotFound from './Components/NotFound/NotFound'
 import Biscuit from './Components/App'
-import DisplayLeaderboard from './Components/LeaderboardDisplay/LeaderboardDisplay'
+import Leaderboard from './Components/Leaderboard'
 
 import * as serviceWorker from './serviceWorker'
 
@@ -15,6 +15,7 @@ class Routing extends React.Component {
         <div>
           <Switch>
           <Route exact path="/" component={ Biscuit } />
+          <Route exact path="/leaderboard" component={ Leaderboard } />
           <Route component={ NotFound } />
           </Switch>
         </div>
