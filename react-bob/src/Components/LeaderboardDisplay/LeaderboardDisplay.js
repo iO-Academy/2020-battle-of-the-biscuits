81c6db74f4957c74a9a0dbdce4dff804b63eaa1e
Hannah Golightly <hannah.golightly@hotmail.co.uk>
2020-06-11 16:02:24 +0100

    styling to leaderboard

--- react-bob/src/Components/LeaderboardDisplay/LeaderboardDisplay.js
+++ react-bob/src/Components/LeaderboardDisplay/LeaderboardDisplay.js
@@ -1,33 +1,47 @@
 import React from 'react'
 import GoToBiscuitBtn from './GoToBiscuitsBtn/GoToBiscuitsBtn'
+import './LeaderboardDisplay.scss'
+
 
 class LeaderboardDisplay extends React.Component {
-  render () {
+  render() {
     let i = 0
     let j = 3
-        return (
-          <div>       
-            <h3>Top Ten Leaderboard</h3>
-            <div className= "leaderboard">
-              <div className="top3">
-                {console.log(this.props.topThreeBiscuits)}
-                {this.props.topThreeBiscuits.map((topThree, index) => {
-                  i++
-                  return <div className={`biscuit${index}`}>{i}{topThree.name}{topThree.winratio}</div>   
-                }
-                )}
-              </div>
-              <div className="fourToTen">
-
-                {this.props.fourToTenBiscuits.map((fourToTen, index) => {
-                  j++
-                  return <div className={`biscuit${index}`}>{j}{fourToTen.name}</div>   
-                }
-                )}
-              </div>
-            </div>
+    return (
+      <div>
+        <h3>Top Ten Leaderboard</h3>
+        <div className="leaderboard">
+          <div className="leaderboard_headings">
+            <p className="position">Position</p>
+            <p className="biscuit_name">Biscuit</p>
+            <p className="win">Win ratio %</p>
           </div>
-        )
+          <div className="top3">
+            {console.log(this.props.topThreeBiscuits)}
+            {this.props.topThreeBiscuits.map((topThree, index) => {
+              i++
+              return <div className="row" key={i}>
+                <div className={`biscuit${index}`}>{i}</div>
+                <div className={`biscuit${index}`}>{topThree.name}</div>
+                <div className={`biscuit${index}`}>{topThree.winratio}</div>
+              </div>
+            }
+            )}
+          </div>
+          <div className="fourToTen">
+            {this.props.fourToTenBiscuits.map((fourToTen, index) => {
+              j++
+              return <div className="row" key={j}>
+                <div className={`biscuit${index}`}>{j}</div>
+                <div className={`biscuit${index}`}>{fourToTen.name}</div>
+                <div className={`biscuit${index}`}>{fourToTen.winratio}</div>
+              </div>
+            }
+            )}
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
