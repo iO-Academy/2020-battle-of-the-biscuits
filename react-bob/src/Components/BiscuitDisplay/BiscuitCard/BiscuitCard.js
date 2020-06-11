f986c68948af39bf3df19a7375079b85d9c3b2ce
Dani Catterill <danicatterill@gmail.com>
2020-06-11 11:39:27 +0100

    added function and onClick event for incrementing comparision and win

--- react-bob/src/Components/BiscuitDisplay/BiscuitCard/BiscuitCard.js
+++ react-bob/src/Components/BiscuitDisplay/BiscuitCard/BiscuitCard.js
@@ -6,7 +6,8 @@ class BiscuitCard extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            biscuitToBeDisplayed: {}
+            biscuitToBeDisplayed: {},
+            selected: false
         }
     }
 
@@ -17,10 +18,34 @@ class BiscuitCard extends React.Component {
             })
         }
     }
+    markSelected = () => {
+        this.setState = ({
+            selected: true
+        })
+    }
+    cardSelected = () => {
+        this.markSelected()
+        // some function to regenarate biscuits
+        if (this.state.selected) {
+            let win = this.state.biscuitToBeDisplayed.wincount++
+            let compared = this.state.biscuitToBeDisplayed.comparisoncount++
+            this.setState = {
+                wincount: win,
+                comparisoncount: compared
+            }
+        } else {
+            let compared = this.state.biscuitToBeDisplayed.comparisoncount++
+            this.setState = {
+                comparisoncount: compared
+            }
+        }
+        console.log(this.state.biscuitToBeDisplayed.comparisoncount)
+        // some function to calculate win ratio?
+    }
 
     render () {
         return (
-            <div className="biscuitCard">
+            <div className="biscuitCard" onClick={ () => { this.cardSelected()} }>
                 <div className="image" style={{backgroundImage: "url(" + this.state.biscuitToBeDisplayed.img + ")"}} alt="biscuit"></div>
                 <h3>{this.state.biscuitToBeDisplayed.name}</h3>
                 <h3>Recommended Dunking Time: {this.state.biscuitToBeDisplayed.RDT} seconds</h3>
