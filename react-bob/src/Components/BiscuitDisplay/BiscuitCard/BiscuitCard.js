a162111e43ceea8b938cd65b3da479fa0ee5724d
Isabella Aston <isabella.aston8@gmail.com>
2020-06-12 09:48:51 +0100

    onclick now working to send info for both cards

--- react-bob/src/Components/BiscuitDisplay/BiscuitCard/BiscuitCard.js
+++ react-bob/src/Components/BiscuitDisplay/BiscuitCard/BiscuitCard.js
@@ -7,9 +7,7 @@ class BiscuitCard extends React.Component {
         super(props)
         this.state = {
             biscuitToBeDisplayed: {},
-            status: "loser"
         }
-        this.markSelected = this.markSelected.bind(this);
     }
 
     componentDidUpdate(prevProps, prevState) {
@@ -24,42 +22,9 @@ class BiscuitCard extends React.Component {
         }
     }
 
-    markSelected = () => {
-        this.props.generateNewCards()
-         this.setState({
-             status: "winner"
-        }, () => {
-            this.sendInfo()
-        })
-    }
-
-    sendInfo = () => {
-        console.log(this.state.status)
-        if (this.state.status == "winner") {
-            this.sendWinner()
-        }
-        // } else {
-            // this.sendLoser()
-        // }
-    }
-
-    sendWinner = () => {
-        fetch("http://localhost:9000/biscuits/winner", {
-            "method": "PUT",
-            "body": JSON.stringify({
-                name: this.state.biscuitToBeDisplayed.name,
-                comparisoncount: this.state.biscuitToBeDisplayed.comparisoncount,
-                wincount: this.state.biscuitToBeDisplayed.wincount
-            })
-        })
-        .then(response => response.json())
-        .then(response => {console.log(response);
-        })
-    }
-
     render () {
         return (
-            <div className="biscuitCard" onClick={this.markSelected}>
+            <div className="biscuitCard" onClick= {this.props.onClick}>
                 <div className="image" style={{backgroundImage: "url(" + this.state.biscuitToBeDisplayed.img + ")"}} alt="biscuit"></div>
                 <h3>{this.state.biscuitToBeDisplayed.name}</h3>
                 <h3>Recommended Dunking Time: {this.state.biscuitToBeDisplayed.RDT} seconds</h3>
--- react-bob/src/Components/BiscuitDisplay/BiscuitDisplay.js
+++ react-bob/src/Components/BiscuitDisplay/BiscuitDisplay.js
@@ -18,14 +18,52 @@ class BiscuitDisplay extends React.Component {
         }
     }
 
+    markSelected = (index, otherIndex) => {
+        this.sendWinner(index)
+        this.sendLoser(otherIndex)
+        this.props.generateNewCards()
+    }
+
+    sendWinner = (index) => {
+        console.log(index)
+        // fetch("http://localhost:9000/biscuits/winner", {
+        //     "method": "PUT",
+        //     "body": JSON.stringify({
+        //         name: this.state.biscuitsToDisplay.[index].name,
+        //         comparisoncount: this.state.biscuitsToDisplay.[index].comparisoncount,
+        //         wincount: this.state.biscuitsToDisplay.[index].wincount
+        //     })
+        // })
+        // .then(response => response.json())
+        // .then(response => {console.log(response);
+        // })
+    }
+    sendLoser = (otherIndex) => {
+        console.log(otherIndex)
+        // fetch("http://localhost:9000/biscuits/winner", {
+        //     "method": "PUT",
+        //     "body": JSON.stringify({
+        //         name: this.state.biscuitsToDisplay.[otherIndex].name,
+        //         comparisoncount: this.state.biscuitsToDisplay.[otherIndex].comparisoncount,
+        //         wincount: this.state.biscuitsToDisplay.[otherIndex].wincount
+        //     })
+        // })
+        // .then(response => response.json())
+        // .then(response => {console.log(response);
+        // })
+    }
+    handleClick() {
+        console.log('Click happened');
+      }
+
     render () {
         return (
             <div className="App">
                 <h3>Click on your favourite</h3>
                 <div className='comparison'>
-                    <BiscuitCard BiscuitSelected={this.state.biscuitsToDisplay[0]} generateNewCards={this.props.generateNewCards}/>
+                    <BiscuitCard BiscuitSelected={ this.state.biscuitsToDisplay[0] } onClick={ () => this.markSelected(0,1) } generateNewCards={ this.props.generateNewCards }/>
                     <h2>VS</h2>
-                    <BiscuitCard BiscuitSelected={this.state.biscuitsToDisplay[1]} generateNewCards={this.props.generateNewCards}/>
+                    <BiscuitCard BiscuitSelected={ this.state.biscuitsToDisplay[1] } onClick={ () => this.markSelected(1,0) } generateNewCards={ this.props.generateNewCards }/>
                 </div>
             </div>
         )
